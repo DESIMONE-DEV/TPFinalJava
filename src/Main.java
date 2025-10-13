b0e75f9ea567c5cc8161c193125b85ed32c03593
emapaez2208 <emapaezspognardi@gmail.com>
2025-10-13 11:56:11 -0300

    testCommit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IO;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Probandoasdasdasda");
+        IO.println("prueba");
     }
 }
