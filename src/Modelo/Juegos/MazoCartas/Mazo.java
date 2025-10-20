ee5e63998012bbbb5d20cbaad20245230dea7773
Ferjs3 <fernandojs120@gmail.com>
2025-10-20 01:59:54 -0300

    Metodos mazo
    Creacion de los metodos del mazo

--- src/Modelo/Juegos/MazoCartas/Mazo.java
+++ src/Modelo/Juegos/MazoCartas/Mazo.java
@@ -1,32 +1,54 @@
 package Modelo.Juegos.MazoCartas;
-import java.security.spec.ECParameterSpec;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
 import Enums.EValorCarta;
 import Enums.EPaloCarta;
 
 public class Mazo {
-    private HashSet<Carta> mazo;
+    private Stack<Carta> mazo;
 
     public Mazo() {
-        mazo = new HashSet<>();
+        mazo = new Stack<>();
         for(EPaloCarta palo: EPaloCarta.values()){
             for(EValorCarta valor: EValorCarta.values()){
-                mazo.add(new Carta(palo, valor.getValorSimb()));
+                mazo.push(new Carta(palo, valor.getValorSimb()));
             }
         }
+    }
+    /// --------------------- Metodos --------------------------------------
+
+    //Funcion shuffle reorganiza el mazo aleatoriamente
+    public void mezclar(){
+        Collections.shuffle(mazo);
+    }
+    //Se manda entero por parametro, y se reparte dicha cantidad
+    public List<Carta> repartir(int cantidad){
+        List<Carta> mano = new ArrayList<>();
+        for(int i = 0; i < cantidad && !mazo.isEmpty(); i++){
+            mano.add(mazo.pop());
+        }
+        return mano;
+    }
+    public Carta agarrarCarta(){
+        if(!mazo.isEmpty()){
+            return mazo.pop();
+        }else {
+            System.out.println("No hay mas cartas");
+            return null;
+        }
+    }
+    public boolean hayCartas(){
+        return !mazo.isEmpty();
     }
     public void verMazo(){
         for(Carta carta: mazo){
             System.out.println(carta);
         }
     }
-    /*
+    /// --------------------- Fin metodos --------------------------------------
 
-    public void repartir();
-    public void mezclar()
-    public void agarrarCarta()
-    public void hayCartas()
-    public void verMano()
 
-     */
 }
