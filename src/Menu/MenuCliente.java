d727946adab2f7b5904017f028d0858249581d5e
Ferjs3 <101979907+Ferjs3@users.noreply.github.com>
2025-10-27 12:33:05 -0300

    Creacion MenuCliente

---
+++ src/Menu/MenuCliente.java
@@ -0,0 +1,23 @@
+package Menu;
+
+import Modelo.Usuarios.Cliente;
+
+public class MenuCliente {
+    public static void menuCliente(Cliente cliente) {
+
+        //jugar
+
+        //cargar
+        //retirar
+        //cambiar cuenta bancaria
+        //cambiar contrasenia
+        //salir
+
+
+    }
+    public static void menuInicial(){
+        System.out.println("1. Lista de juegos");
+        System.out.println("2. Cargar saldo");
+        System.out.println();
+    }
+}
