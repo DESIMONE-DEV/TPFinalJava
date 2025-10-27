08ff1a1e1cc63d8633e854c85c709b2930e32662
Ferjs3 <101979907+Ferjs3@users.noreply.github.com>
2025-10-27 09:53:25 -0300

    MenuRuleta
    Se creo el switch del menu ruleta

--- src/Menu/MenuRuleta.java
+++ src/Menu/MenuRuleta.java
@@ -6,11 +6,6 @@ import Modelo.Usuarios.Cliente;
 import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Scanner;
-
-import Exceptions.ColorNoExisteException;
-import Exceptions.SaldoInsuficienteException;
-import Exceptions.NumeroFueraDeRango;
-
 public class MenuRuleta {
     public static void Start(Cliente jugador) {
 
@@ -37,15 +32,35 @@ public class MenuRuleta {
             System.out.println("4. Columnas ");
             System.out.println("5. MayorMenor ");
             System.out.println("6. ParImpar ");
-            System.out.println("7. Salir");
+            System.out.println("7. Girar ruletas");
+            System.out.println("8. Salir");
 
             opcion = sc.next().charAt(0);
 
             switch (opcion) {
                 case 1:
                     System.out.println("Apuesta plenos:");
-                    //metodo
+                    try {
+                        System.out.println("Ingrese numero al que quiera apostar: ");
+                        int num = sc.nextInt();
+                        sc.nextLine();
+                        if(num < 0 || num > 36){
+                            throw new NumeroFueraDeRango();
+                        }
+                        System.out.println("Ingrese monto a apostar: ");
+                        double monto = sc.nextDouble();
+                        sc.nextLine();
+
+                        ruleta.apostar(String.valueOf(num), monto);
+
+                        System.out.println("Apuesta realizada a " + num);
+
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException();
+                        sc.nextLine();
+                    }
                     break;
+
                 case 2:
                     System.out.println("Apuesta de color:");
                     try {
@@ -61,33 +76,122 @@ public class MenuRuleta {
 
                         ruleta.apostar(color, monto);
                         System.out.println("Apuesta realizada a " + color);
-                    } catch (ColorNoExisteException e) {
-                        System.out.println(/*Mensaje de la excepcion*/);
+
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException();
                         sc.nextLine();
                     }
                     break;
+
                 case 3:
                     System.out.println("Apuesta de docenas:");
-                    //metodo
-                    break;
-                case 4:
-                    System.out.println("Apuesta de columnas:");
-                    //metodo
-                    break;
-                case 5:
-                    System.out.println("Apuesta de mayor o menor:");
-                    //metodo
+                    try {
+                        System.out.println("Ingrese la docena a la que quiera apostar (1 / 2 / 3): ");
+                        int num =  sc.nextInt();
+                        sc.nextLine();
+
+                        String doc = null;
+                        if(num == 1){
+                            doc = "primera docena";
+                        }else if(num == 2){
+                            doc = "segunda docena";
+                        }else if(num == 3){
+                            doc = "tercera docena";
+                        }else{
+                            throw new NumeroFueraDeRango();
+                        }
+
+                        System.out.println("Ingrese monto a apostar: ");
+                        double monto = sc.nextDouble();
+                        sc.nextLine();
+
+                        ruleta.apostar(doc, monto);
+                        System.out.println("Apuesta realizada a " + doc);
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException();
+                        sc.nextLine();
+                    }
                     break;
 
+                case 4:
+                    System.out.println("Apuesta de columnas:");
+                    try {
+                        System.out.println("Ingrese la columna a la que quiera apostar (1 / 2 / 3): ");
+                        int num =  sc.nextInt();
+                        sc.nextLine();
+
+                        String col = null;
+                        if(num == 1){
+                            col = "primera columna";
+                        }else if(num == 2){
+                            col = "segunda columna";
+                        }else if(num == 3){
+                            col = "tercera columna";
+                        }else{
+                            throw new NumeroFueraDeRango();
+                        }
+
+                        System.out.println("Ingrese monto a apostar: ");
+                        double monto = sc.nextDouble();
+                        sc.nextLine();
+
+                        ruleta.apostar(col, monto);
+                        System.out.println("Apuesta realizada a " + col);
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException();
+                        sc.nextLine();
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Apuesta de mayor o menor:");
+                    try {
+                        System.out.println("Escriba si quiere apostar a menor (1 - 18) o mayor (19 - 36): ");
+                        String menorMayor = sc.nextLine().toLowerCase();
+
+                        if(!menorMayor.equals("menor") && (!menorMayor.equals("mayor"))){
+                            throw new DatoIncorrectoException();
+                        }
+                        System.out.println("Ingrese monto a apostar: ");
+                        double monto = sc.nextDouble();
+                        sc.nextLine();
+
+                        ruleta.apostar(menorMayor, monto);
+                        System.out.println("Apuesta realizada a " + menorMayor);
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException();
+                        sc.nextLine();
+                    }
+                    break;
                 case 6:
                     System.out.println("Apuesta de par o impar:");
-                    //metodo
+
+                    try {
+                        System.out.println("Escriba si quiere apostar a par o impar: ");
+                        String parImpar = sc.nextLine().toLowerCase();
+
+                        if(!parImpar.equals("par") && (!parImpar.equals("impar"))){
+                            throw new DatoIncorrectoException();
+                        }
+
+                        System.out.println("Ingrese monto a apostar: ");
+                        double monto = sc.nextDouble();
+                        sc.nextLine();
+
+                        ruleta.apostar(parImpar, monto);
+                        System.out.println("Apuesta realizada a " + parImpar);
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException();
+                        sc.nextLine();
+                    }
                     break;
+                case 7:
+
                 default:
                     System.out.println("Opcion invalida");
             }
-        }while (opcion != '7');
+        }while (opcion != '8');
 
-        }
     }
+}
 
