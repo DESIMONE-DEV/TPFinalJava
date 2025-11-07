a1b900fbaa3fe5f71dceb143fc69547152f658c3
German <germaniyo_18@hotmail.com>
2025-11-07 01:14:15 -0300

    black jack

--- src/Modelo/Juegos/BlackJack.java
+++ src/Modelo/Juegos/BlackJack.java
@@ -56,50 +56,65 @@ public class BlackJack extends Juego implements IRepartidor {
 
     public boolean buscarAs (Carta carta) {
         for (Carta c : banca) {
-            if (c.getValor().equals(as)){
+            if (c.getValor().getValorSimb().equals("A")){
                 return true;
             }
         }
         return false;
     }
-
     public int negra  (Carta carta) {
-        if( carta.getValor().equals(11) || carta.getValor().equals(12) || carta.getValor().equals(13)) {
+        if( carta.getValor().getValorSimb().equals("J")|| carta.getValor().getValorSimb().equals("Q") || carta.getValor().getValorSimb().equals("K")) {
             return 10;
+        }else if (carta.getValor().getValorSimb().equals("A")){
+            return 11;
         }
         return carta.getValor().getValorNum();
     }
-     public boolean agragar () throws MazoVacioException {
+     public boolean agregar () throws MazoVacioException {
         return banca.add((Carta) mazo.repartir(1));
      }
     public int manoBanca () throws MazoVacioException {
         int suma = 0;
         boolean buscar = true;
         do {
-            agragar();
             for (Carta carta : banca) {
                 suma += negra(carta);
                 if (suma > 21 && buscar == buscarAs(carta)) {
                     suma -= 10;
+                    agregar();
                 }
             }
         } while (suma > 17);
         return suma ;
     }
 
-    public int manoJugador (int suma ) throws MazoVacioException {
-
-        boolean buscar = true;
-        agragar();
+    public int manoJugador () throws MazoVacioException {
+        int suma = 0;
+        boolean V = true;
+        agregar();
         for (Carta carta : jugador) {
             suma += negra(carta);
-            if (suma > 21 && buscar == buscarAs(carta)) {
+            if (suma > 21 && V == buscarAs(carta)) {
                 suma -= 10;
             }
         }
         return suma;
     }
-
+    public int pedirCarta (int suma ) throws MazoVacioException {
+        boolean V = true;
+        if(false == mazo.hayCartas()){
+            throw new MazoVacioException();
+        }else {
+            jugador.add((Carta) mazo.repartir(1));
+        }
+        for (Carta carta : jugador) {
+            suma += negra(carta);
+            if (suma > 21 && V == buscarAs(carta)) {
+                suma -= 10;
+            }
+        }
+        return suma;
+    }
 
     @Override
     public void repartir() throws MazoVacioException {
