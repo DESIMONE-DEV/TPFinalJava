b25b5d8fd63e584d84aee2b7c7f366abab4c2601
German <germaniyo_18@hotmail.com>
2025-11-07 14:38:35 -0300

    black jack nuevo

--- src/Modelo/Juegos/MazoCartas/ManoBlackJack.java
+++ src/Modelo/Juegos/MazoCartas/ManoBlackJack.java
@@ -1,9 +1,32 @@
 package Modelo.Juegos.MazoCartas;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ManoBlackJack {
+public class  ManoBlackJack {
     private List<Carta> carta;
+    private double blackJack;
 
+    public ManoBlackJack () {
+        carta = new ArrayList<>();
+        this.blackJack = 0;
+    }
 
+    /// --------------------- GETTERS AND SETTERS -----------------------------
+
+    public double getBlackJack() {
+        return blackJack;
+    }
+
+    public void setBlackJack(double blackJack) {
+        this.blackJack = blackJack;
+    }
+
+    public List<Carta> getCarta() {
+        return carta;
+    }
+
+    public void setCarta(List<Carta> carta) {
+        this.carta = carta;
+    }
 }
