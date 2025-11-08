ab342cbfe2d701bebed936c41e87bc79c0cd6050
German <germaniyo_18@hotmail.com>
2025-11-08 19:34:36 -0300

    acomodando black

--- src/Menu/MenuBlackJack.java
+++ src/Menu/MenuBlackJack.java
@@ -13,10 +13,14 @@ public class MenuBlackJack {
         BlackJack21 blackJack21 = new BlackJack21();
         int opcion = 0;
         String salir = "p";
+        int sumaJugador=0;
+        int sumaBanca=0;
 
+        System.out.println(".............BIENVENIDOS....A....BLACKJACK.......21.........");
+        System.out.println("\n\n\n");
+        dibujoCarta();
         do {
-            System.out.println("1-REPARTIR CARTAS");
-            System.out.println("2-TUSTAR CARTAS");
+            System.out.println("1-COMENZAR EL JEUEGO");
             System.out.println("3-AGREGAR CARTAS");
             System.out.println("4-VERIFICAR QUE SACO LA BANCA");
             System.out.println("5-COMPARA VALORES");
@@ -26,32 +30,55 @@ public class MenuBlackJack {
             switch (opcion) {
                 case 1:
                     try {
+                        System.out.println("SE MEZCLAN LAS CARTAS");
+                        scan.nextLine();
+                        System.out.println("EL PAGADOR REPARTE LAS CARTAS ");
                         blackJack21.repartir();
+
                     } catch (MazoVacioException e) {
                         System.out.println(e.getMessage());
                     }
-                    break;
-
+                    System.out.println("Aprete ENTER para continuar...");
+                    scan.nextLine();
+                    limpiar();
                 case 2:
-                    int suma2 = 0;
+                    System.out.println("..........TUSTAR CARTAS.............");
                     try {
-                        suma2 = blackJack21.manoUsuario();
-                        System.out.println("SACASTE = " + suma2 + " Puntos");
-                        if (suma2 == 21) {
-                            System.out.println("SACASTE BLACKJACK ............... SE PAGA POR EL 150%");
+                        sumaJugador= blackJack21.manoUsuario();
+                        System.out.println("SACASTE = " + sumaJugador+ " Puntos");
+                        if (sumaJugador == 21) {
+                            System.out.println("SACASTE BLACKJACK ............... SE PAGA POR EL 150%\n");
+                            System.out.println("             ...................");
+                            System.out.println("             ......GANADOR......");
+                            System.out.println("             ...................");
                         }
                     } catch (MazoVacioException e) {
                         System.out.println(e.getMessage());
                     }
-                    break;
-                case 3:
-                    int suma3 = 0;
-                    try {
-                        suma3 = blackJack21.pedirCarta();
-                        System.out.println("LA SUMA DE SUS CARTAS ES = " + suma3);
-                    } catch (MazoVacioException e) {
-                        System.out.println(e.getMessage());
+                    if(sumaJugador== 21) {
+                        break;
+                    }else{
+                        System.out.println("\n\nDesea agregar otro carta ?s/n");
+                        salir = scan.nextLine();
+                        if(salir.equals("s")) {
+                            System.out.println("APRETE ESPACIO PARA RECIBIR CARTA");
+                            scan.nextLine();
+                        }else {
+                            break;
+                        }
                     }
+                case 3:
+                    do {
+                        try {
+                            sumaJugador = blackJack21.pedirCarta();
+                            System.out.println("LA SUMA DE SUS CARTAS ES = " + sumaJugador + " Puntos");
+                        } catch (MazoVacioException e) {
+                            System.out.println(e.getMessage());
+                        }
+                        System.out.println("Quiere seguir pidiendo carta? s/n");
+                        String r="f";
+                        r=scan.nextLine();
+                    }while( salir.equalsIgnoreCase("s"));
                     break;
                 case 4:
                     int suma = 0;
@@ -69,6 +96,23 @@ public class MenuBlackJack {
             salir = scan.nextLine();
 
 
-        } while (salir.equalsIgnoreCase( "s"));
+        } while (salir.equalsIgnoreCase("s"));
+    }
+
+    public static void limpiar() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n");
+    }
+
+    public static void dibujoCarta (){
+        System.out.println("+----------+");
+        System.out.println("| A        |");
+        System.out.println("|          |");
+        System.out.println("|    ♣♣    |");
+        System.out.println("|   ♣  ♣   |");
+        System.out.println("|    ♣♣    |");
+        System.out.println("|          |");
+        System.out.println("|        A |");
+        System.out.println("+----------+");
     }
 }
